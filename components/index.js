753c59e8e37a61f14fa39d3f8421f2f8655f3444
Yaflman <hamaouiyaflman@gmail.com>
2024-10-14 15:22:01 +0100

    second commit

--- components/index.js
+++ components/index.js
@@ -1,8 +1,6 @@
 import Navbar from './Navbar';
 import { TypingText, TitleText } from './CustomTexts';
 import ExploreCard from './ExploreCard';
-import StartSteps from './StartSteps';
-import NewFeatures from './NewFeatures';
 import InsightCard from './InsightCard';
 import Footer from './Footer';
 
@@ -10,8 +8,6 @@ export {
   Navbar,
   TitleText,
   TypingText,
-  StartSteps,
-  NewFeatures,
   ExploreCard,
   InsightCard,
   Footer,
